e929f27ef4c2bbd1c67a735626ab98562fcdeeee
Anna Volovenko <anna.volovenko@first-utility.com>
2018-10-17 02:21:15 +0200

    Добавлен класс Distance

--- sandboxAV/src/main/java/ua/av/sanndboxav/JavaProg.java
+++ sandboxAV/src/main/java/ua/av/sanndboxav/JavaProg.java
@@ -5,12 +5,14 @@ public class JavaProg {
   public static void main(String[] args) {
 
    Point p1 = new Point(1,2);
-
    Point p2 = new Point(3, 4);
-
    String s1 = "(" + p1.x + "," + p1.y + ")";
    String s2 = "(" + p2.x + "," + p2.y + ")";
-   System.out.println("Расстояние между точками " + s1 + " и " + s2 + " = " + distance(p1, p2));
+   Distance d = new Distance(1, 2, 3,4);
+
+   System.out.println("Class Point:    Расстояние между точками " + s1 + " и " + s2 + " = " + distance(p1, p2));
+   System.out.println("Class Distance: Расстояние между точками " + d.p1 + " и " + d.p2 + " = " + d.distance());
+
 
   }
 
--- sandboxAV/src/main/java/ua/av/sanndboxav/Point.java
+++ sandboxAV/src/main/java/ua/av/sanndboxav/Point.java
@@ -3,8 +3,10 @@ package ua.av.sanndboxav;
 public class Point {
   int x;
   int y;
+
   public Point (int x, int y) {
     this.x = x;
     this.y = y;
   }
+
 }
