47821cbc17a03ada17c17d0d855dab010fb57769
Anna Volovenko <anna.volovenko@first-utility.com>
2018-10-17 20:34:50 +0200

    Задание решено

--- sandboxAV/src/main/java/ua/av/sanndboxav/JavaProg.java
+++ sandboxAV/src/main/java/ua/av/sanndboxav/JavaProg.java
@@ -5,19 +5,11 @@ public class JavaProg {
   public static void main(String[] args) {
 
    Point p1 = new Point(1,2);
-   Point p2 = new Point(3, 4);
-   String s1 = "(" + p1.x + "," + p1.y + ")";
-   String s2 = "(" + p2.x + "," + p2.y + ")";
-   Distance d = new Distance(1, 2, 3,4);
 
-   System.out.println("Class Point:    Расстояние между точками " + s1 + " и " + s2 + " = " + distance(p1, p2));
-   System.out.println("Class Distance: Расстояние между точками " + d.p1 + " и " + d.p2 + " = " + d.distance());
+   String s1 = "(" + p1.x1 + "," + p1.y1 + ")";
 
+   System.out.println("Расстояние между точками " + s1 + " и (3,4) равно " + p1.distance(3,4));
 
   }
 
-  public static double distance(Point p1, Point p2) {
-    double d = Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
-    return d;
-  }
 }
--- sandboxAV/src/main/java/ua/av/sanndboxav/Point.java
+++ sandboxAV/src/main/java/ua/av/sanndboxav/Point.java
@@ -1,12 +1,17 @@
 package ua.av.sanndboxav;
 
 public class Point {
-  int x;
-  int y;
+  int x1;
+  int y1;
 
-  public Point (int x, int y) {
-    this.x = x;
-    this.y = y;
+  public Point (int x1, int y1) {
+    this.x1 = x1;
+    this.y1 = y1;
   }
+  public double distance (int a, int b) {
+    double d = Math.sqrt((this.x1-a)*(this.x1-a)+(this.y1-b)*(this.y1-b));
+    return d;
+  }
+
 
 }
