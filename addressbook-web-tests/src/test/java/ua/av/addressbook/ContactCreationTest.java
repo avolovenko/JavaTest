761f6be7c30c1a5565e28356521194e8c6c8d9ca
Anna Volovenko <anna.volovenko@first-utility.com>
2018-10-24 02:28:41 +0200

    Перенесение в TestBase оставшихся вспомогательных методов

--- addressbook-web-tests/src/test/java/ua/av/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ua/av/addressbook/ContactCreationTest.java
@@ -1,99 +1,14 @@
 package ua.av.addressbook;
 
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
+import org.testng.annotations.Test;
 
-public class ContactCreationTest {
-  private WebDriver wd;
-
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new ChromeDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    login("admin", "secret");
-  }
+public class ContactCreationTest extends  TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-
     initNewContact();
     fillNewContactData(new ContactData("FirstName", "LastName", "Address street, 5/1, City, PostCode", "697975432", "email@gmail.com", "12", "DECEMBER", "2000"));
     submitNewContact();
-  }
-
-  private void submitNewContact() {
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-  }
-
-  private void fillNewContactData(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData.getPhoneMobile());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmailAddress());
-    wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getbDay());
-    wd.findElement(By.name("bday")).click();
-    wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getbMonth());
-    wd.findElement(By.name("bmonth")).click();
-    wd.findElement(By.name("byear")).click();
-    wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(contactData.getbYear());
-  }
-
-  private void initNewContact() {
-    wd.findElement(By.linkText("ADD_NEW")).click();
-  }
-
-  private void login(String username, String password) {
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='LOGIN']")).click();
-  }
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    logout();
-    wd.quit();
-    }
-
-  private void logout() {
-    wd.findElement( By.linkText( "LOGOUT" ) ).click( );
-  }
-
-
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
   }
 
 }
--- addressbook-web-tests/src/test/java/ua/av/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/av/addressbook/TestBase.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -88,5 +89,38 @@ public class TestBase {
 
   protected void selectGroup() {
     wd.findElement( By.xpath("(//input[@name='selected[]'])[3]")).click();
+  }
+
+  protected void submitNewContact() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  protected void fillNewContactData(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getPhoneMobile());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmailAddress());
+    wd.findElement(By.name("bday")).click();
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getbDay());
+    wd.findElement(By.name("bday")).click();
+    wd.findElement(By.name("bmonth")).click();
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getbMonth());
+    wd.findElement(By.name("bmonth")).click();
+    wd.findElement(By.name("byear")).click();
+    wd.findElement(By.name("byear")).clear();
+    wd.findElement(By.name("byear")).sendKeys(contactData.getbYear());
+  }
+
+  protected void initNewContact() {
+    wd.findElement(By.linkText("ADD_NEW")).click();
   }
 }
