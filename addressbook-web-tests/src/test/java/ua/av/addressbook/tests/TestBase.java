dac5c78bdfae62599d583cf36c92d863b12540ab
Anna Volovenko <anna.volovenko@first-utility.com>
2018-10-30 23:48:13 +0100

    Тесты в Chrome & FF

--- addressbook-web-tests/src/test/java/ua/av/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/av/addressbook/tests/TestBase.java
@@ -4,9 +4,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ua.av.addressbook.appmanager.ApplicationManager;
 
+import static org.openqa.selenium.remote.BrowserType.CHROME;
+import static org.openqa.selenium.remote.BrowserType.FIREFOX;
+import static org.openqa.selenium.remote.BrowserType.IE;
+
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager( );
+  protected final ApplicationManager app = new ApplicationManager( CHROME );
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
