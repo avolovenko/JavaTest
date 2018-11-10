7e75cea62d410ca1e885f1c505e55462899abcdd
Anna Volovenko <anna.volovenko@first-utility.com>
2018-11-11 00:19:57 +0100

    Сортировка списков

--- addressbook-web-tests/src/test/java/ua/av/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ua/av/addressbook/model/GroupData.java
@@ -3,12 +3,16 @@ package ua.av.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private final String id;
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  private int id;
   private final String gName;
   private final String gHeader;
   private final String gFooter;
 
-  public GroupData(String id,String gName, String gHeader, String gFooter) {
+  public GroupData(int id,String gName, String gHeader, String gFooter) {
     this.id = id;
     this.gName = gName;
     this.gHeader = gHeader;
@@ -16,15 +20,13 @@ public class GroupData {
   }
 
   public GroupData(String gName, String gHeader, String gFooter) {
-    this.id = null;
+    this.id = Integer.MAX_VALUE;
     this.gName = gName;
     this.gHeader = gHeader;
     this.gFooter = gFooter;
   }
 
-  public String getId() {
-    return id;
-  }
+  public int getId() {return id;  }
 
   public String getgName() {
     return gName;
@@ -39,25 +41,23 @@ public class GroupData {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass( ) != o.getClass( )) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals( id, groupData.id ) &&
-            Objects.equals( gName, groupData.gName );
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash( id, gName );
-  }
-
-  @Override
   public String toString() {
     return "GroupData{" +
             "Id='" + id + '\'' +
             ", gName='" + gName + '\'' +
             '}';
+  }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass( ) != o.getClass( )) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals( gName, groupData.gName );
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash( gName );
   }
 
 }
--- addressbook-web-tests/src/test/java/ua/av/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ua/av/addressbook/tests/GroupCreationTest.java
@@ -1,9 +1,11 @@
 package ua.av.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ua.av.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTest extends TestBase {
@@ -12,9 +14,16 @@ public class GroupCreationTest extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper( ).gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper( ).createGroup( new GroupData("Group1", "GroupHeader1", "GroupFooter1") );
+    GroupData group = new GroupData( "Group1", "GroupHeader1", "GroupFooter1");
+    app.getGroupHelper( ).createGroup( group );
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() +1);
+
+    before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare( g1.getId(), g2.getId() );
+    before.sort( byId );
+    after.sort( byId );
+    Assert.assertEquals( before, after );
   }
 
 }
