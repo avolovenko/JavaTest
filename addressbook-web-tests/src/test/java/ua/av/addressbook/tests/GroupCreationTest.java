1daa014ad82cd360fac778846b1bbfdb23e54c2a
Anna Volovenko <anna.volovenko@first-utility.com>
2018-11-10 19:59:09 +0100

    Контроль количества элементов групп

--- addressbook-web-tests/src/test/java/ua/av/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ua/av/addressbook/tests/GroupCreationTest.java
@@ -1,5 +1,6 @@
 package ua.av.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ua.av.addressbook.model.GroupData;
 
@@ -8,7 +9,10 @@ public class GroupCreationTest extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper( ).gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper( ).createGroup( new GroupData("Group1", "GroupHeader1", "GroupFooter1") );
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before +1);
   }
 
 }
--- addressbook-web-tests/src/test/java/ua/av/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ua/av/addressbook/tests/GroupModificationTest.java
@@ -1,5 +1,6 @@
 package ua.av.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.av.addressbook.model.GroupData;
 
@@ -8,6 +9,7 @@ public class GroupModificationTest extends TestBase {
   @Test
   public void testGroupModification () {
     app.getNavigationHelper( ).gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("Group1", "GroupHeader1", "GroupFooter1"));
     }
@@ -16,5 +18,7 @@ public class GroupModificationTest extends TestBase {
     app.getGroupHelper( ).fillGroupForm(new GroupData("UpdatedGroup", "UpdatedGroupHeader", "UpdatedGroupFooter"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper( ).returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals( after, before  );
   }
 }
