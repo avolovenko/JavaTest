7a02682ae32ad2555f1503601e6f416f1ce6a2a2
Anna Volovenko <anna.volovenko@first-utility.com>
2018-11-29 23:48:12 +0100

    Object Relation Mapping

--- addressbook-web-tests/src/test/java/ua/av/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ua/av/addressbook/model/GroupData.java
@@ -3,18 +3,35 @@ package ua.av.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @XStreamAlias("group")
+@Entity
+@Table(name="group_list")
 public class GroupData {
   @XStreamOmitField
+  @Id
+  @Column(name = "group_id")
   private int id = Integer.MAX_VALUE;
+
   @Expose
+  @Column(name = "group_name")
   private String gName;
+
   @Expose
+  @Column(name = "group_header")
+  @Type(type="text")
   private String gHeader;
+
   @Expose
+  @Column(name = "group_footer")
+  @Type(type="text")
   private String gFooter;
 
   public GroupData withId(int id) {
