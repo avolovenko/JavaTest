26760f2179bf5e5b1c6aa31dbbdef055ecebd2c9
Anna Volovenko <anna.volovenko@first-utility.com>
2018-12-15 21:50:16 +0100

    8.7

--- mantis-tests/src/test/java/ua/annav/mantis/Tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ua/annav/mantis/Tests/RegistrationTests.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ua.annav.mantis.model.MailMessage;
 
+import javax.mail.MessagingException;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -15,13 +16,13 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
-  @BeforeMethod
+  //@BeforeMethod
   public void startMailServer() {
     app.mail().start();
   }
 
   @Test
-  public void TestRegistration() throws IOException {
+  public void TestRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
     String user = String.format("user%s", now);
     String password = "password";
@@ -30,8 +31,11 @@ public class RegistrationTests extends TestBase {
     /*String user = "user1";
     String password = "password";
     String email = "user1@email.com";*/
+
+    app.james().createUser(user, password);
     app.registration().start(user, email);
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user, password));
@@ -43,7 +47,7 @@ public class RegistrationTests extends TestBase {
     return regex.getText(mailMessage.text);
   }
 
-  @AfterMethod (alwaysRun = true)
+  //@AfterMethod (alwaysRun = true)
   public void stopMailServer() {
     app.mail().stop();
   }
